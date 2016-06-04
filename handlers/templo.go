fe766de3aea6b0fc1a222de8f91abd1dceb67987
Antonio Pagano <apagano@wawand.co>
2016-06-04 10:14:50 -0500

    [cleaning]

--- handlers/templo.go
+++ handlers/templo.go
@@ -1,15 +1,20 @@
 package handlers
 
+import (
+	"log"
+	"net/http"
+)
+
 //Templo is a sample handler to show off some of the components of the template
-// func Templo(w http.ResponseWriter, r *http.Request) {
-// 	view, err := views.GetTemplate("templo.jet")
-// 	if err != nil {
-// 		log.Println("Unexpected template err:", err.Error())
-// 		return
-// 	}
-//
-// 	name := r.URL.Query().Get(":name")
-// 	view.Execute(w, nil, struct {
-// 		Name string
-// 	}{name})
-// }
+func Templo(w http.ResponseWriter, r *http.Request) {
+	view, err := views.GetTemplate("templo.jet")
+	if err != nil {
+		log.Println("Unexpected template err:", err.Error())
+		return
+	}
+
+	name := r.URL.Query().Get(":name")
+	view.Execute(w, nil, struct {
+		Name string
+	}{name})
+}
--- handlers/templo_test.go
+++ handlers/templo_test.go
@@ -1,16 +1,25 @@
 package handlers
 
-// func TestTemplo(t *testing.T) {
-// 	router := muxFor("/hi/:name", Templo)
-// 	request, _ := http.NewRequest("GET", "/hi/Tony", nil)
-// 	response := httptest.NewRecorder()
-//
-// 	router.ServeHTTP(response, request)
-// 	assert.Contains(t, string(response.Body.Bytes()), "Hello Tony")
-// }
-//
-// func muxFor(path string, handler http.HandlerFunc) *pat.PatternServeMux {
-// 	router := pat.New()
-// 	router.Get(path, http.HandlerFunc(handler))
-// 	return router
-// }
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/bmizerany/pat"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTemplo(t *testing.T) {
+	router := muxFor("/hi/:name", Templo)
+	request, _ := http.NewRequest("GET", "/hi/Tony", nil)
+	response := httptest.NewRecorder()
+
+	router.ServeHTTP(response, request)
+	assert.Contains(t, string(response.Body.Bytes()), "Hello Tony")
+}
+
+func muxFor(path string, handler http.HandlerFunc) *pat.PatternServeMux {
+	router := pat.New()
+	router.Get(path, http.HandlerFunc(handler))
+	return router
+}
